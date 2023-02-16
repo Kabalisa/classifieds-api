e96e1334a26efb84076c8febd13c014a68559e56
Innocent fiston Kabalisa <kabalisafiston@Innocents-MacBook-Pro.local>
2023-02-16 14:14:20 +0200

    signin endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,11 +2,36 @@ import { Request, Response } from "express";
 import { Seller } from "../models/seller";
 import { BadRequestError } from "../errors";
 import { Token } from "../services/token";
+import { Password } from "../services/password";
 
 class AuthController {
   static async signin(req: Request, res: Response) {
-    return res.send(true);
+    const { phoneNumber, password } = req.body;
+
+    const existingSeller = await Seller.findOne({ phoneNumber });
+
+    if (!existingSeller) {
+      throw new BadRequestError("seller does not exist");
+    }
+
+    const passwordMatch = await Password.compare(
+      password,
+      existingSeller.password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid password");
+    }
+
+    const jwt = Token.jwtSign(
+      existingSeller.id,
+      existingSeller.name,
+      existingSeller.phoneNumber
+    );
+
+    return res.send({ seller: existingSeller, jwt });
   }
+
   static async signup(req: Request, res: Response) {
     const { name, phoneNumber, password } = req.body;
 
