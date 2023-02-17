9e6147fd381c360577a0ae6103490a9b8f55db08
Innocent fiston Kabalisa <kabalisafiston@Innocents-MacBook-Pro.local>
2023-02-17 08:43:26 +0200

    update get products endpoint

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -6,11 +6,12 @@ class ProductController {
     const product = Product.build({ ...req.body, userId: req.currentUser?.id });
 
     await product.save();
-    return res.send({product});
+    return res.send({ product });
   }
   static async getProducts(req: Request, res: Response) {
-    const products = await Product.find({});
-    return res.send(products);
+    const products = await Product.find({}).collation({locale: "en"}).sort({ name: "asc" });
+    const reducedProducts = products.slice(0, 10);
+    return res.send({ products: reducedProducts });
   }
 }
 
